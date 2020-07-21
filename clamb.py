bdccd500c311740ca2542b8ca5ff920987091488
cw chen <sky85245630@gmail.com>
2020-07-21 23:29:47 +0800

    用key-value取得網頁中的元素資料

--- clamb.py
+++ clamb.py
@@ -6,14 +6,18 @@ resp = requests.get('https://sky85245630.github.io/waterfall-html/')
 soup = BeautifulSoup(resp.text, 'html5lib')
 
 # 印出這個網址裡面“h1”的純文字
-print(soup.find('h1').text)
+# print(soup.find('h1').text)
 
-# 用for找到裡面所有的a物件
-for asd in soup.find_all('div'):
-    print(asd)
-    # 找到div裡面的a連結
-    print(asd.a)
+# # 用for找到裡面所有的a物件
+# for asd in soup.find_all('div'):
+#     print(asd)
+#     # 找到div裡面的a連結
+#     print(asd.a)
 
 # 找到span裡面的waterfall_text class
-for zxc in soup.find_all('span','waterfall_text'):
-    print(zxc)
+# for zxc in soup.find_all('span','waterfall_text'):
+#     print(zxc)
+
+#用key-value取得網頁中的元素資料
+for qwe in soup.find_all('span',{'class':'waterfall_text'}):
+    print(qwe)
